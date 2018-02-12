1f9f0d635f047bcf0822137cf4c650f1dc234108
Seth McAllister <dozingducklet@gmail.com>
2018-02-12 23:02:03 +0000

    More user messages

---
+++ src/main/java/net/extus/messaging/user/UserUpdateMessage.java
@@ -0,0 +1,23 @@
+package net.extus.messaging.user;
+
+import xyz.sethy.extus.api.user.User;
+
+import java.util.UUID;
+
+public class UserUpdateMessage {
+    private final UUID uuid;
+    private final User user;
+
+    public UserUpdateMessage(final UUID uuid, final User user) {
+        this.uuid = uuid;
+        this.user = user;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
